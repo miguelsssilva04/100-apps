d96031c4f63bbc507ee299556cff354448e1db3c
miguelsssilva04 <miguel.silva020104@gmail.com>
2024-06-26 17:00:08 +0100

    start js files

--- fx calculator/position-size.js
+++ fx calculator/position-size.js
@@ -45,13 +45,6 @@ pscButton.addEventListener('dblclick', function(){
 })
 
 
-
-//Collase options once we open a window
-
-
-
-
-
 //Close window
 
 const closeButton = document.querySelector('.x-div');
@@ -64,3 +57,56 @@ closeButton.addEventListener('click', function(){
         windowCalculator.style.display = 'flex';
     }
 })
+
+//Calculator Function
+
+function calculatePositionSize() {
+    const accountCurrency = document.getElementById('account-currency').value;
+    const accountSize = parseFloat(document.getElementById('account-size').value);
+    const riskPercentage = parseFloat(document.getElementById('rr-ratio-%').value);
+    const stopLossSize = parseFloat(document.getElementById('account-currency').value);
+    const currencyPair = document.getElementById('currency-pair').value;
+
+    const riskAmount = accountSize * (riskPercentage/100)
+
+    let pipValue;
+    if (currencyPair === "eurusd" || currencyPair === "gbpusd" || currencyPair === "nzdusd"|| currencyPair === "audusd") {
+        if (accountCurrency === "USD") {
+            pipValue = 10; //For standard lot
+        } else if(accountCurrency === "EUR"){
+            pipValue = 10*1.07;
+        } else if(accountCurrency === "GBP"){
+            pipValue = 10*1.27;
+        }
+    } else if (currencyPair === "usdjpy"){
+        if (accountCurrency === "USD"){
+            pipValue = 6.25;
+        }else if (accountCurrency === "EUR"){
+            pipValue = 6.25*1.07;
+        }else if (accountCurrency === "GBP"){
+            pipValue = 6.25*1.27;
+        }
+
+    } else if (currencyPair === "usdchf"){
+        if (accountCurrency === "USD"){
+            pipValue = 11.14;
+        } else if(accountCurrency === "EUR") {
+            pipValue = 11.14 * 1.07;
+        } else if (accountCurrency === "GBP"){
+            pipValue = 11.14 *1.27;
+        }
+    }else if (currencyPair === "usdcad") {
+        if (accountCurrency === "USD"){
+            pipValue = 7.31;
+        } else if (accountCurrency === "EUR"){
+            pipValue = 7.31*1.07;
+        } else if (accountCurrency === "GBP"){
+            pipValue = 7.31*1.07;
+        }
+    }
+    //Calcular position size
+    const positionSize = riskAmount / (stopLossSize * pipValue);
+
+    document.getElementById('calculator-value').textContent = '${positionSize}'
+
+}
